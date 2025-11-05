9423739dca4ecffe519fcc10687f67e6f0faf709
TAYllefert10 <84572602+TAYllefert10@users.noreply.github.com>
2025-11-05 23:52:03 +0100

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-const GAS_BASE = 'https://script.google.com/macros/s/AKfycbxY9LXTVt8mYrb91rVvSNVrW-eIv2PEJ8YU3sZGTCGucKmElzv05SIiOoxUcxw8kNWG3w/exec?admin=1';
+const GAS_BASE = 'https://script.google.com/macros/s/AKfycbzVq_I14J_087w9xjHU8YIoU65BQytX9se2fqAR6VZ028sne-JEW7tuMT4iu3IiYnIhtA/exec';
 
 const heroBg = document.querySelector('[data-parallax]');
 window.addEventListener('scroll', () => {
